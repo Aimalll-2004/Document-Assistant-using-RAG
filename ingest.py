e360d9fe72c6c6f5bca18bdee420cc8e7abab1ee
Aimalll-2004 <aimaljaved.0@gmail.com>
2026-04-30 13:37:45 +0200

    support for PDF files added

--- ingest.py
+++ ingest.py
@@ -1,22 +1,33 @@
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-loader = DirectoryLoader(
+all_docs = []
+
+
+#TXT loader
+txt_loader = DirectoryLoader(
     "data/documents",
     glob="**/*.txt",
     loader_cls=TextLoader
 )
+all_docs.extend(txt_loader.load())
 
-documents = loader.load()
+#PDF loader
+pdf_loader = DirectoryLoader(
+    "data/documents",
+    glob="**/*.pdf",
+    loader_cls=PyPDFLoader
+)
+all_docs.extend(pdf_loader.load())
 
 splitter = CharacterTextSplitter(
-    chunk_size=500,
+    chunk_size=750,
     chunk_overlap=100
 )
 
-docs = splitter.split_documents(documents)
+docs = splitter.split_documents(all_docs)
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
